dd5261f9650969c9b1c7ebfb5ef5fb877146265e
alvgm98 <alvgm1998@gmail.com>
2024-02-20 17:40:19 +0100

    Mejoras en el modal: - Arreglada la visibilidad del modal. - Ahora cuando el modal se muestra el fondo queda opacado por un overlay oscuro. - Ahora el modal tiene una animación fade-out cuando se cierra.

--- assets/modal.js
+++ assets/modal.js
@@ -1,3 +1,11 @@
+document.getElementById("modalComoJugar").show();
+document.getElementById("overlay-oscuro").style.display = "block";
+
 function cerrarModal() {
-   document.getElementById("modalComoJugar").style = "display: none";
+   document.getElementById("modalComoJugar").className = "fade-out-animation";
+   document
+      .getElementById("overlay-oscuro")
+      .classList.add("fade-out-animation");
+
+   setTimeout(() => document.getElementById("modalComoJugar").close(), 800);
 }
